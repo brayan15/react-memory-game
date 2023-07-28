16b9899715adc5157d4395131d80623d04de73a9
Bryan Lara <bralavar@gmail.com>
2023-07-28 09:09:40 -0500

    feature: add flipped styles

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent } from 'react'
+
 import { CardItem } from 'src/types'
+import img from 'src/assets/question-mark.png'
 
 type CardT = {
   card: CardItem
@@ -7,12 +9,21 @@ type CardT = {
 
 const Card: FunctionComponent<CardT> = ({ card }) => {
   return (
-    <div className="card-item h-100">
-      <img
-        src={card.fields.image.url}
-        alt={card.fields.image.title}
-        className="card-item__image m-auto d-block border border-dark rounded p-1"
-      />
+    <div className="card-item">
+      <div className="card-item__image-container card-item__image-container-front">
+        <img
+          src={img}
+          alt="question mark"
+          className="card-item__image m-auto d-block border border-dark rounded p-1"
+        />
+      </div>
+      <div className="card-item__image-container card-item__image-container-back">
+        <img
+          src={card.fields.image.url}
+          alt={card.fields.image.title}
+          className="card-item__image m-auto d-block border border-dark rounded p-1"
+        />
+      </div>
     </div>
   )
 }
